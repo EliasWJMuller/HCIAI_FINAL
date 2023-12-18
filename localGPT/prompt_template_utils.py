6704e7bd0b0206ff088d358d3b8a2e5104f6ab43
CroopSchmelle <95125813+CroopSchmelle@users.noreply.github.com>
2023-12-18 23:07:15 +0100

    final commit

---
+++ localGPT/prompt_template_utils.py
@@ -0,0 +1,94 @@
+"""
+This file implements prompt template for llama based models. 
+Modify the prompt template based on the model you select. 
+This seems to have significant impact on the output of the LLM.
+"""
+
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+
+# this is specific to Llama-2.
+
+system_prompt = """You are an active participant in a virtual roundtable discussion. Focus on responding to the user prompt. The discussion will include a prompt
+and responses already contributed by other AIs. Follow these guidelines for effective participation:
+
+Thoroughly review all provided context, including the user question and existing responses, before formulating your answer.
+Base your responses strictly on the information given by the user and the dialogue from other participants. Refrain from using external or 
+additional sources. Approach each query in a step-by-step manner, ensuring logical progression and clarity in your answers. Critically assess the 
+contributions from others before you. If a query has not yet been addressed, take the initiative to provide a well-thought-out initial response.
+Only contribute to the subject matter itself. Critically assess the correctness of the previous answer. You must rate it on a numerical scale of 1-10."""
+
+
+
+def get_prompt_template(system_prompt=system_prompt, promptTemplate_type=None, history=False):
+    if promptTemplate_type == "llama":
+        B_INST, E_INST = "[INST]", "[/INST]"
+        B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+        SYSTEM_PROMPT = B_SYS + system_prompt + E_SYS
+        if history:
+            instruction = """
+            Context: {history} \n {context}
+            User: {question}"""
+
+            prompt_template = B_INST + SYSTEM_PROMPT + instruction + E_INST
+            prompt = PromptTemplate(input_variables=["history", "context", "question"], template=prompt_template)
+        else:
+            instruction = """
+            Context: {context}
+            User: {question}"""
+
+            prompt_template = B_INST + SYSTEM_PROMPT + instruction + E_INST
+            prompt = PromptTemplate(input_variables=["context", "question"], template=prompt_template)
+    elif promptTemplate_type == "mistral":
+        B_INST, E_INST = "<s>[INST] ", " [/INST]"
+        if history:
+            prompt_template = (
+                B_INST
+                + system_prompt
+                + """
+    
+            Context: {history} \n {context}
+            User: {question}"""
+                + E_INST
+            )
+            prompt = PromptTemplate(input_variables=["history", "context", "question"], template=prompt_template)
+        else:
+            prompt_template = (
+                B_INST
+                + system_prompt
+                + """
+            
+            Context: {context}
+            User: {question}"""
+                + E_INST
+            )
+            prompt = PromptTemplate(input_variables=["context", "question"], template=prompt_template)
+    else:
+        # change this based on the model you have selected.
+        if history:
+            prompt_template = (
+                system_prompt
+                + """
+    
+            Context: {history} \n {context}
+            User: {question}
+            Answer:"""
+            )
+            prompt = PromptTemplate(input_variables=["history", "context", "question"], template=prompt_template)
+        else:
+            prompt_template = (
+                system_prompt
+                + """
+            
+            Context: {context}
+            User: {question}
+            Answer:"""
+            )
+            prompt = PromptTemplate(input_variables=["context", "question"], template=prompt_template)
+
+    memory = ConversationBufferMemory(input_key="question", memory_key="history")
+
+    return (
+        prompt,
+        memory,
+    )
